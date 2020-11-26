aa935a335c9de575bbcdb6f429c70d44033f5b59
Neha Ghanta <neha.ghanta@workinman.com>
2020-11-26 12:53:37 -0600

    Player Movement
    Created Player Movement script and implemented movement animations to player

---
+++ Fighting Game/Assets/Scripts/Camera Scripts/PlayerFollower.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollower : MonoBehaviour
+{
+    [SerializeField]
+    private Transform playerTransform;
+
+    [SerializeField]
+    private Vector3 offsetPosition;
+
+    [SerializeField]
+    private Space offsetPositionSpace = Space.Self;
+
+    [SerializeField]
+    private bool lookAt = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        if (playerTransform == null)
+        {
+            Debug.LogWarning("Missing target ref", this);
+            return;
+        }
+
+        // position computation
+        if (offsetPositionSpace == Space.Self)
+        {
+            transform.position = playerTransform.TransformPoint(offsetPosition);
+        }
+        else
+        {
+            transform.position = playerTransform.position + offsetPosition;
+        }
+
+        // rotation computation
+        if (lookAt)
+        {
+            transform.LookAt(playerTransform);
+        }
+        else
+        {
+            transform.rotation = playerTransform.rotation;
+        }
+
+    }
+}
---
+++ Fighting Game/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    CharacterController characterController;
+
+    [SerializeField]
+    Animator playerAnimator;
+
+    public float speed = 9.0f;
+    public float rotateSpeed = 30f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        characterController.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.up * rotateSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            playerAnimator.SetBool("Forward", true);
+            //playerAnimator.SetFloat("Velocity", characterController.velocity.magnitude);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            playerAnimator.SetBool("Forward", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            playerAnimator.SetBool("Backwards", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            playerAnimator.SetBool("Backwards", false);
+        }
+        playerAnimator.SetInteger("Velocity", Mathf.Abs((int) characterController.velocity.magnitude));
+    }
+}
